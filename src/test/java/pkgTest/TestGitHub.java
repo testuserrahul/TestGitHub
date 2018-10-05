14d0263d0dc65fbd79d435673128b274802d1a49
testuserrahul <43461028+testuserrahul@users.noreply.github.com>
2018-10-04 22:28:52 -0500

    commit changes

---
+++ src/test/java/pkgTest/TestGitHub.java
@@ -0,0 +1,16 @@
+package pkgTest;
+
+import org.testng.annotations.Test;
+
+public class TestGitHub {
+	
+	@Test
+	public void test(){
+		System.out.println("Hello Selenium");
+		System.out.println("Hello Selenium");
+		System.out.println("Hello Selenium");
+		System.out.println("Hello Selenium");
+		System.out.println("Hello Selenium");
+	}
+
+}
